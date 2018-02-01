1a790fee15c93d255f67b9331c9185bac9b1a27d
alumno <alumno@D3B04>
2018-02-01 11:18:50 -0600

    Volumen

--- Playback/MainWindow.xaml.cs
+++ Playback/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Playback
     public partial class MainWindow : Window
     {
         private Mp3FileReader reader;
-        private WaveOut output;
+        private WaveOutEvent output;
         DispatcherTimer timer;
         bool dragging = false;
 
@@ -85,7 +85,7 @@ namespace Playback
         {
             if(output != null && output.PlaybackState == PlaybackState.Paused)
             {
-                output.Resume();
+                output.Play();
                 btnPlay.IsEnabled = false;
                 btnPause.IsEnabled = true;
                 btnStop.IsEnabled = true;
@@ -96,9 +96,16 @@ namespace Playback
             }
             if (txtRuta.Text != null && txtRuta.Text != "")
             {
-                output = new WaveOut();
+                output = new WaveOutEvent();
                 output.PlaybackStopped += OnPlaybackStop;
                 reader = new Mp3FileReader(txtRuta.Text);
+
+                //Configuraciones de WaveOut
+                output.DeviceNumber = cbDispositivos.SelectedIndex;
+                output.NumberOfBuffers = 2;
+                output.DesiredLatency = 100;
+                output.Volume = (float)sldVolumen.Value;
+
                 output.Init(reader);
                 output.Play();
                 btnStop.IsEnabled = true;
@@ -161,6 +168,16 @@ private void btnStop_Click(object sender, RoutedEventArgs e)
                     btnStop.IsEnabled = false;
                     btnPlay.IsEnabled = true;
                 }
+            }
+        }
+
+        
+
+        private void sldVolumen_DragCompleted(object sender, RoutedEventArgs e)
+        {
+            if (output != null)
+            {
+                output.Volume = (float)sldVolumen.Value;
             }
         }
     }
